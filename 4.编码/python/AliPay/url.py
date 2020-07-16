1f1980e90c3894484848f0a02b267f8a5876d887
Iconzjy <38494400+Iconzjy@users.noreply.github.com>
2020-07-16 20:19:14 +0800

    upload

--- 4.编码/python/AliPay/url.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from view import alipaview
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    # 支付宝返回url
-    url(r'^alipa/', alipaview, name='alipa'),
-]
